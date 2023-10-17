9811fe0d56251cf34146debff2dcca1a04e28df0
IrvinMitre <mitre_ismm@outlook.com>
2023-10-17 17:55:42 -0600

    Feature: Initialize project with basic configuration files

---
+++ src/App.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello, Stori!!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
